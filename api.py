986e0130e2c5f6d520eaf95b76c39611f142493f
maxence <maxence.adihou@gmail.com>
2024-11-16 20:01:09 +0100

    feat - debut weatherrequester

---
+++ api.py
@@ -0,0 +1,36 @@
+import os
+import requests
+from constant import *
+from dotenv import load_dotenv
+
+
+class WeatherRequester:
+    load_dotenv()
+
+    def __init__(self):
+        self._api_key = os.getenv("API_KEY")
+
+    def get_weather_by_location(self, latitude: float, longitude: float, units: str = "celcius", lang: str = "fr") -> dict:
+        """
+        Methode permettant d'obtenir la méteo actuelle par localisation geographique(latitude, longitude).
+        :param latitude: Latitude de l'emplacement géographique [-90; 90]
+        :param longitude: Longitude de l'emplacement géographique [-180; 180]
+        :param units: Unité de mesure de la températude. Unités disponibles: celcius (défaut), fahrenheit, kelvin
+        :param lang: Language des données requêtées (Français par défaut)
+        :return:
+        """
+        url = end_point + f"lat={latitude}&lon={longitude}&lang={lang}&appid={self._api_key}&units={UNITS[units]}"
+        response = requests.get(url=url)
+        return response.json()
+
+    def get_weather_by_city_name(self, city_name: str, units: str = "celcius", lang: str = "fr") -> dict:
+        """
+
+        :param city_name:
+        :param units:
+        :param lang:
+        :return:
+        """
+        url = end_point + f"q={city_name}&lang={lang}&appid={self._api_key}&units={UNITS[units]}"
+        response = requests.get(url=url)
+        return response.json()
---
+++ constant.py
@@ -0,0 +1,2 @@
+end_point = "https://api.openweathermap.org/data/2.5/weather?"
+UNITS = {"celcius": "metric", "fahrenheit": "imperial", "kelvin": "standard"}
